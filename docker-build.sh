b03a8a8d78951e51f40ffc00919157fbc15eca9c
Alex Dolski <alexd@n-ary.net>
2020-10-30 15:04:38 -0500

    Tag the image with the Cantaloupe version in addition to `latest`

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,7 @@
 
 source ./env.sh
 
-docker build -t $APP_NAME .
+ZIP_FILE=$(ls image_files/cantaloupe-*)
+VERSION=$(echo $ZIP_FILE | sed -En 's/image_files\/cantaloupe-(.+).zip$/\1/p')
+
+docker build -t $APP_NAME -t $APP_NAME:$VERSION .
