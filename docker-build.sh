e6e83783a1f72bbca78eabacea0fa6b23151f9c0
Alex Dolski <alexd@n-ary.net>
2021-03-24 13:22:43 -0500

    Fix docker build invocation

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,4 @@
 
 source ./env.sh
 
-ZIP_FILE=$(ls image_files/cantaloupe-*)
-VERSION=$(echo $ZIP_FILE | sed -En 's/image_files\/cantaloupe-(.+).zip$/\1/p')
-
-docker build -t $APP_NAME -t $APP_NAME:$CANTALOUPE_VERSION .
+docker build -t $APP_NAME:$CANTALOUPE_VERSION .
