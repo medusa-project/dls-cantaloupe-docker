8d9ef5831871bdda08dbf1537f1c01f19c37fe86
Alex Dolski <alexd@n-ary.net>
2021-04-01 12:46:31 -0500

    Fix behavior when the cwd has spaces in it

--- repackage.sh
+++ repackage.sh
@@ -7,7 +7,7 @@ CWD=$(pwd)
 CANTALOUPE_DIR=../../cantaloupe-project/cantaloupe
 cd $CANTALOUPE_DIR
 mvn clean package -DskipTests
-cd $CWD
+cd "$CWD"
 rm ./image_files/cantaloupe-*.zip
 cp $CANTALOUPE_DIR/target/cantaloupe-*.zip ./image_files
 ./docker-build.sh
